faa065762ad27c979a08f2aba11ccefc5659d145
svgupta-eng <svgupta@ucsd.edu>
2026-04-24 16:07:46 -0700

    edited global.js

--- global.js
+++ global.js
@@ -63,3 +63,36 @@ select.addEventListener('input', function (event) {
 if ("colorScheme" in localStorage) {
     setColorScheme(localStorage.colorScheme);
 }
+
+export async function fetchJSON(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
+
+export function renderProjects(project, containerElement, headingLevel = 'h2') {
+  containerElement.innerHTML = '';
+  
+  for (let p of project) {
+    const article = document.createElement('article');
+    
+    article.innerHTML = `
+      <${headingLevel}>${p.title}</${headingLevel}>
+      <img src="${p.image}" alt="${p.title}">
+      <p>${p.description}</p>
+    `;
+    
+    containerElement.appendChild(article);
+  }
+}
+
+export async function fetchGitHubData(username) {
+  return fetchJSON(`https://api.github.com/users/${username}`);
+}
